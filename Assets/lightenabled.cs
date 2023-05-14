0708209a2cf2a711e0c0fdf4feaafe7f6539ab70
bytesandbots <students@bytesandbots.com>
2023-05-14 15:02:18 -0700

    added sound fx

---
+++ Assets/lightenabled.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightenabled : MonoBehaviour
+{
+    public Light Cactuslight;
+    public AudioSource cactusbing;
+    private bool alreadyhit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cactuslight.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("rollyball") && !alreadyhit)
+        {
+            Cactuslight.enabled = true;
+            cactusbing.Play();
+            alreadyhit = true;
+        }
+    }
+}
