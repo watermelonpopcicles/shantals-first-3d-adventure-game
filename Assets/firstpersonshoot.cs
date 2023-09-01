d1c65adbce10ecb1f5120e1a307c0b598f8ca86d
Fernando de Paz <f.depaz@hotmail.com>
2023-08-31 18:46:36 -0700

    need to finish shooting

---
+++ Assets/firstpersonshoot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstpersonshoot : MonoBehaviour
+{
+    public float speedX = 270;
+    public float speedY = 270;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Mouse X") * speedX;
+        float v = Input.GetAxis("Mouse Y") * -speedY;
+
+
+        Vector3 mov = new Vector3(v, h, 0);
+        transform.Rotate(mov * Time.deltaTime);
+
+        Vector3 temp = transform.localEulerAngles;
+        temp.z = 0;
+        transform.localEulerAngles = temp;
+    }
+}
