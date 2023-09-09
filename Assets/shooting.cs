74630b9c87fbfe06ae4b0114a2a36b020ffabe3c
Fernando de Paz <f.depaz@hotmail.com>
2023-09-08 19:14:52 -0700

    finish shooting script

---
+++ Assets/shooting.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public GameObject arrow;
+    public Transform arrowspawn;
+    public float allowedshotspersec;
+    public float strength;
+    private float ctime; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ctime >= allowedshotspersec) 
+        {
+            if (Input.GetMouseButton(0)) 
+            {
+                GameObject clone = Instantiate(arrow, arrowspawn.position, arrowspawn.rotation);
+                ctime 
+            }
+        }   
+    }
+}
