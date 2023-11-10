c94f72672a6c4bfcc8d4657f5c794149d03a0a58
Fernando de Paz <f.depaz@hotmail.com>
2023-11-09 19:17:32 -0800

    Update zoombow.cs

--- Assets/zoombow.cs
+++ Assets/zoombow.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class zoombow : MonoBehaviour
 {
+    public GameObject bow;
+    public Transform zoom;
+    public Transform notzoom;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bow.transform.position = notzoom.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(1)) 
+        { 
+            bow.transform.position = zoom.position;
+        }
+        else
+        {
+            bow.transform.position = notzoom.position;
+        }
     }
 }
